7e9513bbe87bc5ba7ddc72f0bebb70692b734553
Chubbray <chubbray1@gmail.com>
2020-05-29 17:17:55 -0600

    created JS

---
+++ js/at.js
@@ -0,0 +1,31 @@
+var date = $("#date");
+var saveButton = $(".save-Btn");
+var event = $(".event");
+var timeBlock = $(".time-block");
+
+function setStatus() {
+    hour = moment().hours();
+    timeBlock.each(function (){
+        var thisHour = parseInt($(this).attr("id"));
+        if (thisHour > hour){
+            $((this).children[1]).addClass("future")
+        }
+        else if (thisHour === hour){
+            $((this).children[1]).addClass("present")
+        }
+        else {
+            $((this).children[1]).addClass("past")
+        }
+    })
+}
+
+setStatus();
+
+function setPlaner() {
+    var timer = setInterval(function (){
+        var currentDate = moment().format("dddd, MMMM, YYYY");
+        date.text(currentDate);
+    }, 1000);
+
+}
+setPlaner();
